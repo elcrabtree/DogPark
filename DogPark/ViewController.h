15c18d7531afa86512571ff628fbc7ac3fc9cea4
Mike Crabtree <mike.cr@btree.us>
2013-03-09 14:27:58 -0600

    Initial Commit

---
+++ DogPark/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DogPark
+//
+//  Created by Michael Crabtree on 3/9/13.
+//  Copyright (c) 2013 GlitterExplosions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DogPark/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DogPark
+//
+//  Created by Michael Crabtree on 3/9/13.
+//  Copyright (c) 2013 GlitterExplosions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
